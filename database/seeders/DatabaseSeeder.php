8e1c962bae9c7b746e8f4e7881867c2af05e5fdb
john <john.castillo@eliteacesinc.com>
2024-10-02 16:22:25 +0800

    uncomment address seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             CartSeeder::class,
             CartItemSeeder::class,
             OrderItemSeeder::class,
-            AddressSeeder::class,
+//            AddressSeeder::class,
         ]);
 
     }
