3cc6958dd2021ed379c81e9585c40a2ed0d7cbe9
John <johnzunigacastillo@gmail.com>
2024-10-07 09:55:16 +0800

    fix seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRole;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -23,6 +24,7 @@ class UserSeeder extends Seeder
                 'email_verified_at' => Carbon::now()->format('Y-m-d H:m'),
                 'completed' => true,
                 'verified' => true,
+                'role' => UserRole::ADMIN
             ],
         ]);
 
